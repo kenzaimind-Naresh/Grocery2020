46e8533d70d7d5d0d132bd3c2528751403920c02
Kenzai Mind <59043711+kenzaimind-Naresh@users.noreply.github.com>
2020-01-01 20:32:51 +0530

    test

---
+++ Skeleton demo test/test/unit/skeleton/NexmoControllerSpec.groovy
@@ -0,0 +1,20 @@
+package skeleton
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(NexmoController)
+class NexmoControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
