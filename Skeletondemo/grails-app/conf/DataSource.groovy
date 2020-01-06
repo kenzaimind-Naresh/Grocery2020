1de887be9ca474e493ede0dda2c39c351df56b36
Kenzai Mind <59043711+kenzaimind-Naresh@users.noreply.github.com>
2020-01-06 11:18:25 +0530

    DataSource.groovy

--- Skeletondemo/grails-app/conf/DataSource.groovy
+++ Skeletondemo/grails-app/conf/DataSource.groovy
@@ -18,7 +18,7 @@ environments {
 	development {
 		dataSource {
 			dbCreate = "update" // one of 'create', 'create-drop','update'
-			url = "jdbc:mysql://localhost:3306/skeletondemotest"
+			url = "jdbc:mysql://localhost:3306/skeletondemo"
 			username = "root"
 			password = "ecg123"
 		}
@@ -27,7 +27,7 @@ environments {
 	test {
 		dataSource {
 			dbCreate = "create" // one of 'create', 'create-drop','update'
-			url = "jdbc:mysql://localhost:3306/skeletondemotest"
+			url = "jdbc:mysql://localhost:3306/skeletondemo"
 			username = "root"
 			password = "ecg123"
 		}
@@ -37,7 +37,7 @@ environments {
 	production {
 		dataSource {
 			dbCreate = "update"
-			url = "jdbc:mysql://localhost:3306/skeletondemotest"
+			url = "jdbc:mysql://localhost:3306/skeletondemo"
 			username = "root"
 			password = "ecg123"
 		}
