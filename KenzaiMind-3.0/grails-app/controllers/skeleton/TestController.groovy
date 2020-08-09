75871f452acf49218c4ec9ab8a3cd18238e6a26f
Kenzai Mind <59043711+kenzaimind-Naresh@users.noreply.github.com>
2020-08-09 14:29:38 +0530

    paytm updates

--- KenzaiMind-3.0/grails-app/controllers/skeleton/TestController.groovy
+++ KenzaiMind-3.0/grails-app/controllers/skeleton/TestController.groovy
@@ -86,6 +86,107 @@ class TestController {
 		log.info("TestController edit Action")
     }
 	
+	
+		def paytmpay2(orderId,customerId,mobileNumber,custEmail,taxAmount) {
+		log.info("TestController paytm2 Action")
+		//orderId,customerId,mobileNumber,custEmail,taxAmount
+		
+		def orderId=orderId;
+		def customerId=customerId;
+		def mobileNumber=mobileNumber;
+		def custEmail=custEmail;
+		def taxAmount=taxAmount;
+				
+		log.info("Order Id :"+orderId);
+		log.info("Customer Id :"+customerId);
+		log.info("Customer Mobile number :"+mobileNumber);
+		log.info("Customer email: "+custEmail);
+		log.info(" Transaction Amount :" +taxAmount);
+		
+		
+		
+		/* initialize a TreeMap object */
+		TreeMap<String, String> paytmParams = new TreeMap<String, String>();
+		
+		/* Find your MID in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys */
+		//paytmParams.put("MID", "FJyzZS17862127734203");
+		paytmParams.put("MID", "kfvcFU91325011852224");
+		
+		/* Find your WEBSITE in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys */
+		paytmParams.put("WEBSITE", "WEBSTAGING");
+		
+		/* Find your INDUSTRY_TYPE_ID in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys */
+		paytmParams.put("INDUSTRY_TYPE_ID", "Retail");
+		
+		/* WEB for website and WAP for Mobile-websites or App */
+		paytmParams.put("CHANNEL_ID", "WEB");
+		
+		/* Enter your unique order id */
+		paytmParams.put("ORDER_ID", orderId);
+		
+		/* unique id that belongs to your customer */
+		paytmParams.put("CUST_ID", customerId);
+		
+		/* customer's mobile number */
+		paytmParams.put("MOBILE_NO", mobileNumber);
+		
+		/* customer's email */
+		paytmParams.put("EMAIL", custEmail);
+		
+		/**
+		* Amount in INR that is payble by customer
+		* this should be numeric with optionally having two decimal points
+		*/
+		paytmParams.put("TXN_AMOUNT", taxAmount);
+		
+		/* on completion of transaction, we will send you the response on this URL */
+		paytmParams.put("CALLBACK_URL", "http://localhost:8080/Skeleton/");
+		
+		/**
+		* Generate checksum for parameters we have
+		* You can get Checksum JAR from https://developer.paytm.com/docs/checksum/
+		* Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys
+		*/
+		//String checksum = checkSumServiceHelper.getCheckSumServiceHelper().genrateCheckSum("3cSNtT3reNGebU6P", paytmParams);
+		String checksum = checkSumServiceHelper.getCheckSumServiceHelper().genrateCheckSum("pOzscXM5eOFfZ@SZ", paytmParams);
+		
+		/* for Staging */
+		String url = "https://securegw-stage.paytm.in/order/process";
+		
+		/* for Production */
+		// String url = "https://securegw.paytm.in/order/process";
+		PrintWriter writer = response.getWriter();
+		/* Prepare HTML Form and Submit to Paytm */
+		StringBuilder outputHtml = new StringBuilder();
+		outputHtml.append("<html>");
+		outputHtml.append("<head>");
+		outputHtml.append("<title>Merchant Checkout Page</title>");
+		outputHtml.append("</head>");
+		outputHtml.append("<body>");
+		outputHtml.append("<center><h1>Please do not refresh this page...</h1></center>");
+		outputHtml.append("<form method='post' action='" + url + "' name='paytm_form'>");
+		
+		for(Map.Entry<String,String> entry : paytmParams.entrySet()) {
+			outputHtml.append("<input type='hidden' name='" + entry.getKey() + "' value='" + entry.getValue() + "'>");
+		}
+		
+		outputHtml.append("<input type='hidden' name='CHECKSUMHASH' value='" + checksum + "'>");
+		outputHtml.append("</form>");
+		outputHtml.append("<script type='text/javascript'>");
+		outputHtml.append("document.paytm_form.submit();");
+		outputHtml.append("</script>");
+		outputHtml.append("</body>");
+		outputHtml.append("</html>");
+		
+		
+		writer.println(outputHtml);
+
+log.info("end of the code")
+//[values:values]
+		render outputHtml;
+		return
+	}
+	
 	def paytmpay() {
 		log.info("TestController paytm Action")
 		
